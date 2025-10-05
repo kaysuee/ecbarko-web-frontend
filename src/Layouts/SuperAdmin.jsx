65df4529a9d30a20468f15617e7fbe01afd752fe
Aliyah Picante <picantealiyahfrance@gmail.com>
2025-10-05 21:33:20 +0800

    make the layout responsive to the sidebar collapse

--- src/Layouts/SuperAdmin.jsx
+++ src/Layouts/SuperAdmin.jsx
@@ -1,9 +1,25 @@
 import React, { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import Sidebar, { SidebarProvider } from '../components/superAdmin/Sidebar'
+import Sidebar, { SidebarProvider, useSidebar } from '../components/superAdmin/Sidebar'
 import Topbar from '../components/superAdmin/Topbar'
 import '../styles/superAdmin-layout.css'
+
+function SuperAdminContent() {
+  const { isCollapsed } = useSidebar();
+  
+  return (
+    <div className='superAdmin-layout'>
+      <Sidebar />
+      <div className={`main-content ${isCollapsed ? 'sidebar-collapsed' : ''}`}>
+        <Topbar />
+        <div className='outlet-content'>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function SuperAdminLayout() {
   const user = useSelector((state) => state.Auth.user)
@@ -17,15 +33,7 @@ export default function SuperAdminLayout() {
 
   return (
     <SidebarProvider>
-      <div className='superAdmin-layout'>
-        <Sidebar />
-        <div className='main-content'>
-          <Topbar />
-          <div className='outlet-content'>
-            <Outlet />
-          </div>
-        </div>
-      </div>
+      <SuperAdminContent />
     </SidebarProvider>
   )
 }
